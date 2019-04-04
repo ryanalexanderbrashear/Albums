1c82025bc4141d47a954f276eb57d3edd26ac734
Alex Brashear <alexbrashear@gmail.com>
2019-04-04 09:50:00 -0400

    Got a component to render

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-/**
- * @format
- */
+//Import a library to help create a component
+import React from 'react';
+//import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native'; //Destructuring the React Native import to only pull what we need from it
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+//Create a component
+const App = () => { //App is the name of our component here
+    return (
+        <Text>Some Text</Text> //JSX -> Extension of JS used to create React components
+    );
+};
 
-AppRegistry.registerComponent(appName, () => App);
+//Render that component
+AppRegistry.registerComponent('albums', () => App); //First argument matches with the project name, second argument is a fat arrow function returning the component to be rendered
+
+
