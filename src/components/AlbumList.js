70f581fb309a61e855e51c79b1afb28b5ca08444
Alex Brashear <alexbrashear@gmail.com>
2019-04-05 11:22:22 -0400

    Added Axios for handling API calls. Set up the basic API call to get the albums and print them to the console

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+//Importing Axios to carry out HTTP calls
+import Axios from 'axios';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text>Album List</Text>
-        </View>
-    );
-};
+class AlbumList extends Component { //Class-based components extend component to have access to the base class information
+    
+    //Life Cycle Methods
+    componentWillMount() { //Executed when the component is about to be rendered to the screen
+        console.log("Component will mount in AlbumList");
+
+        //Use promises to execute code when an asynch call is complete
+        Axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
+
+
+        //debugger; You can use debugger statements to stop execution for debugging
+    }
+    
+    render() {
+        return (
+            <View>
+                <Text>Album List</Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList;
