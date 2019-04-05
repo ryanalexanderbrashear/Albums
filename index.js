e6c720c285337ec85497af35a45cfa9eeec17b4a
Alex Brashear <alexbrashear@gmail.com>
2019-04-05 09:41:22 -0400

    Created the AlbumList component base and set the App component to use it

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 //Import a library to help create a component
 import React from 'react';
 //import ReactNative from 'react-native';
-import { AppRegistry } from 'react-native'; //Destructuring the React Native import to only pull what we need from it
-import Header from './src/components/header'; //Import the Header component by using the file path to Header.js
+import { AppRegistry, View } from 'react-native'; //Destructuring the React Native import to only pull what we need from it
+import Header from './src/components/Header'; //Import the Header component by using the file path to Header.js
+import AlbumList from './src/components/AlbumList';
 
 //Create a component
 const App = () => { //App is the name of our component here, ONLY MAKE ONE COMPONENT PER FILE
     return (
-        <Header headerText={'Albums'} /> //JSX -> Extension of JS used to create React components. Also, header is self-closing tag so we don't need a closing </Header> and can use the self closing tag syntax.
+        //JSX -> Extension of JS used to create React components. Also, header is self-closing tag so we don't need a closing </Header> and can use the self closing tag syntax.
         //This is also an example of passing in a props object to a component.
+        //Only one JSX object can be returned when creating a component, so if we want to use multiple components we need to wrap them in a view tag
+        <View>
+            <Header headerText={'Albums'} />
+            <AlbumList />
+        </View>
     );
 };
 
