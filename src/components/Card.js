dbbbc294210e003e0adbe8e42b3f14c70ff7b907
Alex Brashear <alexbrashear@gmail.com>
2019-04-08 11:37:21 -0400

    Added the Card component to the AlbumDetail component. Created the CardSection component and added boilerplate

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { View } from 'react-native';
 import { declaredPredicate } from '@babel/types';
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <View style={styles.containerStyle}>
-            
+        //Components automatically receive all children that are placed within them (see AlbumDetail) as part of the props that are passed to the component
+        <View style={styles.containerStyle}> 
+            {props.children} 
         </View>
     );
 };
@@ -20,7 +21,7 @@ const styles = {
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.1,
         shadowRadius: 2,
-        elevation, 1,
+        elevation: 1,
         marginLeft: 5,
         marginRight: 5,
         marginTop: 10 
